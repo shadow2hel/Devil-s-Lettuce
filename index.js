3533a1de6d9b93add3709f9ca72697cde84fddf9
antoniocarrano <antonio.carrano@student.kdg.be>
2018-11-17 19:23:10 +0100

    Added prefix and help command

--- index.js
+++ index.js
@@ -2,15 +2,65 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const config = require('./config.json');
 const credentials = require('./credentials.json');
+const fs = require('fs');
+
 
 client.on('ready', () => {
 	console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', msg => {
-	if (msg.content === 'ping') {
-		msg.reply('pong');
+	if(msg.author == client.user) {
+		return;
+	}
+
+	if(msg.content.startsWith(config.prefix)) {
+		processCommand(msg);
 	}
 });
+
+function writeToConfig(value, replaceValue) {
+	fs.readFile('config.json', 'utf8', function readFileCallback(err, data) {
+		if (err) {
+			console.log(err);
+		}
+		else {
+	    obj = JSON.parse(data); // now it an object
+	    obj[value] = replaceValue; // add some data
+	    json = JSON.stringify(obj); // convert it back to json
+			fs.writeFile('config.json', json, (err) => {
+  			if (err) throw err;
+  			console.log('Written to config!');
+			}); // write it back
+		}
+	});
+}
+
+function processCommand(msg) {
+	const fullCommand = msg.content.substr(config.prefix.length).toLowerCase();
+	const splitCommand = fullCommand.split(' ');
+	const primaryCommand = splitCommand[0];
+	const arguments = splitCommand.slice(1, splitCommand.length);
+	msg.reply(arguments);
+
+	switch(primaryCommand) {
+	case 'help':
+		msg.reply('You called for help?');
+		break;
+	case 'prefix':
+		if(arguments.length == 1) {
+			config.prefix = arguments[0];
+			writeToConfig('prefix', arguments[0])
+			msg.reply('You\'ve changed the prefix to ' + arguments[0]);
+		}
+		else {
+			msg.reply('Invalid amount of arguments!');
+		}
+		break;
+	default:
+		msg.reply('Invalid command, refer to ' + config.prefix + 'help for help');
+		break;
+	}
+}
 
 client.login(credentials.token);
